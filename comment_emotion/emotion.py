c8a628ef81a247cd42f82089fc2544a2cb6200fe
hklhai <hkhai@outlook.com>
2018-07-18 16:09:50 +0800

    LD改造，评论存在数据分布量不均匀

--- comment_emotion/emotion.py
+++ comment_emotion/emotion.py
@@ -3,43 +3,89 @@
 
 import codecs
 import json
+import os
 
 import jieba
 from gensim import corpora
 from gensim.models import LdaModel
 
+topic_num = 10
+text = ""
 train = []
 stopwords = codecs.open('chineseStopWords.txt', 'r', encoding='utf8').readlines()
 stopwords = [w.strip() for w in stopwords]
 
+
 # 读取分词结果
 # fp = codecs.open('wiki.zh.seg.utf.txt', 'r', encoding='utf8')
 
-topic_num = 3
-text = ""
-with open('/home/hadoop/comment/2018-07-16') as file_object:
-    for line in file_object:
-        article = json.loads(line)
-        my_text = " ".join(jieba.cut(article['content'], cut_all=False, HMM=True)) + "\n"
-        print(my_text)
-        text += my_text
+def file_list(file_dir):
+    for files in os.walk(file_dir):
+        return files[2]
 
-# print(my_text)
-# for line in text:
-#     line = line.split()
-#     train.append([w for w in line if w not in stopwords])
-line = text.split(" ")
-train.append([w for w in line if w not in stopwords])
 
-dictionary = corpora.Dictionary(train)
-corpus = [dictionary.doc2bow(text) for text in train]
-lda = LdaModel(corpus=corpus, id2word=dictionary, num_topics=topic_num)
+# def lda_output(text, train):
+#     line_list = text.split(" ")
+#     train.append([w for w in line_list if w not in stopwords])
+#
+#     dictionary = corpora.Dictionary(train)
+#     corpus = [dictionary.doc2bow(train_text) for train_text in train]
+#     lda = LdaModel(corpus=corpus, id2word=dictionary, num_topics=topic_num)
+#     lda.save('comment_lda.model')
+#
+#     # 打印前20个topic的词分布
+#     topics = lda.print_topics(topic_num)
+#
+#     # 打印id为20的topic的词分布
+#     for i in range(topic_num):
+#         print(str(i) + ":" + topics[i][1])
+#     print("===============================")
+#
+#
+# file_name = "/home/hadoop/comment"
+# lists = file_list(file_name)
+# for i in range(len(lists)):
+#     print(lists[i])
+#     file_name = lists[i]
+#     with open('/home/hadoop/comment/' + file_name) as file_object:
+#         for line in file_object:
+#             article = json.loads(line)
+#             my_text = " ".join(jieba.cut(article['content'], cut_all=False, HMM=True))
+#             text += my_text
+#
+# lda_output(text, [])
 
-# 打印前20个topic的词分布
-topics = lda.print_topics(topic_num)
-# 打印id为20的topic的词分布
-for i in range(topic_num ):
-    print(str(i) + ":" + topics[i][1])
+
+def lda_output(text, train):
+    line_list = text.split(" ")
+    train.append([w for w in line_list if w not in stopwords])
+
+    dictionary = corpora.Dictionary(train)
+    corpus = [dictionary.doc2bow(train_text) for train_text in train]
+    lda = LdaModel(corpus=corpus, id2word=dictionary, num_topics=topic_num)
+    lda.save('news_lda.model')
+
+    # 打印前20个topic的词分布
+    topics = lda.print_topics(topic_num)
+
+    # 打印id为20的topic的词分布
+    for i in range(topic_num):
+        print(str(i) + ":" + topics[i][1])
+    print("===============================")
+
+
+file_name = "/home/hadoop/news"
+lists = file_list(file_name)
+for i in range(len(lists)):
+    print(lists[i])
+    file_name = lists[i]
+    with open('/home/hadoop/news/' + file_name) as file_object:
+        for line in file_object:
+            article = json.loads(line)
+            my_text = " ".join(jieba.cut(article['content'], cut_all=False, HMM=True))
+            text += my_text
+
+lda_output(text, [])
 
 # lda.save('zhwiki_lda.model')
 # lda = models.ldamodel.LdaModel.load('zhwiki_lda.model')
